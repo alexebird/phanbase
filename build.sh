1fdea14883654b52c95112b0a83bfc3e5198628e
Alex Bird <alexebird@gmail.com>
2015-03-29 00:02:16 -0700

    rename to french

--- build.sh
+++ build.sh
@@ -6,6 +6,7 @@ IN='.'
 #set -vx
 
 mkdir -p "./${OUT}"
+VERSION=$(cat "${IN}/manifest.json" | grep \"version\" | cut -f4 -d' ' | tr -d \",)
 
 cp -r "${IN}/icons" "${OUT}"
 cp -r "${IN}/javascript" "${OUT}"
@@ -15,4 +16,4 @@ cat "${IN}/secrets.js" > "${OUT}/javascript/bg.js.tmp"
 cat "${IN}/secrets.js" "${IN}/javascript/bg.js" > "${OUT}/javascript/bg.js.tmp"
 mv "${OUT}/javascript/bg.js.tmp" "${OUT}/javascript/bg.js"
 find "${OUT}" -name '.DS_Store' | xargs rm -fv
-zip -r phishtracks-buddy.zip "${OUT}"
+zip -r "french-${VERSION}.zip" "${OUT}"
